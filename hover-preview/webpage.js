27716a44eed24b6b3b8c61d5ce33f3d3cec48756
Kevin Filanowski <kevinfilanowski@gmail.com>
2025-05-24 20:38:20 -0700

    testing.

--- hover-preview/webpage.js
+++ hover-preview/webpage.js
@@ -1,9 +1,9 @@
 // Extract repository name from the current URL
-const currentPath = window.location.pathname;
-const pathSegments = currentPath.split('/').filter(part => part);
+const path = window.location.pathname;
+const pathSegments = path.split('/').filter(part => part);
 const repoName = pathSegments.length > 0 ? pathSegments[0] : '';
 
 // If the path doesn't start with the repository name, add it
-if (repoName && !n.pathname.startsWith('/' + repoName)) {
-  n.pathname = '/' + repoName + n.pathname;
+if (repoName && !window.location.pathname.startsWith('/' + repoName)) {
+  window.location.pathname = '/' + repoName + window.location.pathname;
 } 
